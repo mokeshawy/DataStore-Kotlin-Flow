7225482ee35a4ee3eed28eed4264891c9b5a3276
mohamed <mohamed_gsm@msn.com>
2021-09-02 07:32:28 +0200

    finish

--- app/src/main/java/com/example/datastorekotlin_flow/ui/mainactivity/MainActivity.kt
+++ app/src/main/java/com/example/datastorekotlin_flow/ui/mainactivity/MainActivity.kt
@@ -66,7 +66,7 @@ class MainActivity : AppCompatActivity() {
             }
 
             override fun onFinish() {
-                binding.tvTimer.text = "done!"
+                binding.tvTimer.text = "done.."
             }
         }.start()
     }
